55d75c206ba4782f70805c0e6e3c9014a3a36dc3
Felipe Morais <felipemsx18@gmail.com>
2025-11-03 13:58:26 -0300

    added and removed some files

--- projects/forum/web/src/app.tsx
+++ projects/forum/web/src/app.tsx
@@ -3,15 +3,17 @@ import "./global.css";
 import { Routes, Route, Link } from "react-router-dom";
 
 import About from "./pages/about/page";
+import Home from "./pages/home/page";
 
 function App() {
     return (
         <div>
             <nav>
-                <Link to="/about">About</Link> |
+                <Link to="/about">About</Link>
             </nav>
             <Routes>
                 <Route path="/about" element={<About />} />
+                <Route path="/home" element={<Home />} />
             </Routes>
         </div>
     );
